2a9168b75e086f36c63a849fae14b921af79b642
Merlin Walter <m.walter@grandcoding.com>
2020-05-28 13:34:09 +0200

    first commit

---
+++ terraform-provider-infoblox-master/README.md
@@ -0,0 +1,59 @@
+#### NOTE: This Repo is deprecated please use the official provider[https://github.com/terraform-providers/terraform-provider-infoblox] repo to for contributions/Issues/PRs.
+
+# Terraform Provider for Infoblox
+ <img width="171" alt="capture" src="https://user-images.githubusercontent.com/36291746/39614422-6b653088-4f8d-11e8-83fd-05b18ca974a2.PNG">
+
+## Requirements
+* [Terraform](https://www.terraform.io/downloads.html) 0.12.x or greater
+* [Go](https://golang.org/doc/install) 1.12.x (to build the provider plugin)
+* CNA License need to be installed on NIOS. If CNA is not installed then following default EA's should be added in NIOS side:
+   * VM Name :: String Type
+   * VM ID :: String Type
+   * Tenant ID :: String Type
+   * CMP Type :: String Type
+   * Cloud API Owned :: List Type (Values True, False)
+   * Network Name :: String Type
+
+## Building the Provider
+Clone repository and enter the provider directory and build the provider.
+
+_**Note:** Ensure you do not clone to $GOPATH/src or below (as was normal prior to Go 1.11)_
+
+```sh
+$ git clone https://github.com/infobloxopen/terraform-provider-infoblox
+$ cd terraform-provider-infoblox
+$ make build
+```
+
+## Using the Provider
+If you're building the provider, follow the instructions to [install it as a plugin](https://www.terraform.io/docs/plugins/basics.html#installing-a-plugin). After the build is complete, copy the `terraform-provider-infoblox` binary into the same path as your terraform binary. After placing it into your plugins directory, run `terraform init` to initialize it.
+
+## Developing the Provider
+If you wish to work on the provider, you'll first need Go installed on your machine (version 1.12+ is required).
+
+To compile the provider, run the following steps:
+```sh
+$ make build
+...
+$ ./terraform-provider-infoblox
+...
+```
+To test the provider, you can simply run `make test`.
+```sh
+$ make test
+```
+
+In order to run the full suite of acceptance tests `make testacc`.
+```sh
+$ make testacc
+```
+## Features of Provider
+* Creation of Network View in NIOS appliance
+* Creation & Deletion of Network in NIOS appliance
+* Allocation & Deallocation of IP from a Network
+* Association & Disassociation of IP Address for a VM
+* Creation and Deletion of A, CNAME, Host, and Ptr records
+
+## Disclaimer
+To use the provider for DNS purposes, a parent (i.e. zone) must already exist. The plugin does not support the creation of zones.
+while running acceptance tests create a 10.0.0.0/24 network under default network view and create a reservation for 10.0.0.2 IP
---
+++ terraform-provider-infoblox-master/infoblox/provider.go
@@ -0,0 +1,107 @@
+package infoblox
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/infobloxopen/infoblox-go-client"
+	"time"
+)
+
+//Provider returns a terraform.ResourceProvider.
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"server": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("INFOBLOX_SERVER", nil),
+				Description: "Infoblox server IP address.",
+			},
+			"username": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("INFOBLOX_USERNAME", nil),
+				Description: "User to authenticate with Infoblox server.",
+			},
+			"password": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("INFOBLOX_PASSWORD", nil),
+				Description: "Password to authenticate with Infoblox server.",
+			},
+			"wapi_version": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("WAPI_VERSION", "2.7"),
+				Description: "WAPI Version of Infoblox server defaults to v2.7.",
+			},
+			"port": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("PORT", "443"),
+				Description: "Port number used for connection for Infoblox Server.",
+			},
+
+			"sslmode": &schema.Schema{
+				Type:        schema.TypeBool,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SSLMODE", "false"),
+				Description: "If set, Infoblox client will permit unverifiable SSL certificates.",
+			},
+			"connect_timeout": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("CONNECT_TIMEOUT", 60),
+				Description: "Maximum wait for connection, in seconds. Zero or not specified means wait indefinitely.",
+			},
+			"pool_connections": &schema.Schema{
+				Type:        schema.TypeInt,
+				Optional:    true,
+				DefaultFunc: schema.EnvDefaultFunc("POOL_CONNECTIONS", "10"),
+				Description: "Maximum number of connections to establish to the Infoblox server. Zero means unlimited.",
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"infoblox_network":        resourceNetwork(),
+			"infoblox_network_view":   resourceNetworkView(),
+			"infoblox_ip_allocation":  resourceIPAllocation(),
+			"infoblox_ip_association": resourceIPAssociation(),
+			"infoblox_a_record":       resourceARecord(),
+			"infoblox_cname_record":   resourceCNAMERecord(),
+			"infoblox_ptr_record":     resourcePTRRecord(),
+		},
+		DataSourcesMap: map[string]*schema.Resource{
+			"infoblox_network": dataSourceNetwork(),
+		},
+		ConfigureFunc: providerConfigure,
+	}
+
+}
+
+func providerConfigure(d *schema.ResourceData) (interface{}, error) {
+
+	var seconds int64
+	seconds = int64(d.Get("connect_timeout").(int))
+	hostConfig := ibclient.HostConfig{
+		Host:     d.Get("server").(string),
+		Port:     d.Get("port").(string),
+		Username: d.Get("username").(string),
+		Password: d.Get("password").(string),
+		Version:  d.Get("wapi_version").(string),
+	}
+
+	transportConfig := ibclient.TransportConfig{
+		SslVerify:           d.Get("sslmode").(bool),
+		HttpRequestTimeout:  time.Duration(seconds),
+		HttpPoolConnections: d.Get("pool_connections").(int),
+	}
+
+	requestBuilder := &ibclient.WapiRequestBuilder{}
+	requestor := &ibclient.WapiHttpRequestor{}
+
+	conn, err := ibclient.NewConnector(hostConfig, transportConfig, requestBuilder, requestor)
+	if err != nil {
+		return nil, err
+	}
+	return conn, err
+}
